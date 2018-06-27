0b1b725754c6f5fc16e566e1eea495d3a3936bfa
sushant12 <sussyoung9@gmail.com>
2018-06-27 19:14:33 +0545

    a working codebase

--- geo.py
+++ geo.py
@@ -2,17 +2,39 @@
 
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 import exifread as ef
+import os, csv, time
 
 window = tk.Tk()
 window.title("GEO Extractor")
 window.geometry("400x200")
 
 def open_callback():
-    print(open_folder())
+    folder = open_folder()
+    files = os.listdir(open_folder())
+    total_files = len(files)
+    progress_bar = ttk.Progressbar(orient="horizontal",
+                    length=int(total_files), mode="determinate")
+    progress_bar.grid( row=2, column=1, sticky="nsew")
+    progress_bar['value'] = 0
+    filename = int(round(time.time() * 1000))
+    i = 1
+    for file in files:
+        with open(folder+"/"+file, 'rb') as f:
+            tags = ef.process_file(f)
+            if "GPS GPSLatitude" in tags and "GPS GPSLongitude" in tags:
+                lat = _convert_to_degress(tags.get('GPS GPSLatitude'))
+                lon = _convert_to_degress(tags.get('GPS GPSLongitude'))
+                coordinates = [lat, lon]
+                with open(str(filename)+'.csv', 'a+') as gps_file:
+                    wr = csv.writer(gps_file, quoting=csv.QUOTE_ALL)
+                    wr.writerow(coordinates)
+                    progress_bar['value'] = i
+        i += 1
 
 def open_folder():
-    folder = tk.filedialog.askdirectory(initialdir = "/", title = "Select a folder")
+    folder = tk.filedialog.askdirectory(title = "Select a folder")
     return folder
 
 def _convert_to_degress(value):
